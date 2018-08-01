3073b4b79659b92a44d26e9ff33bc8dd41747f4d
Anirudh Maddula <aam252@cornell.edu>
2018-08-01 01:03:14 -0500

    dynamic member thing working. style is so broken though

--- src/info/teamdata.jsx
+++ src/info/teamdata.jsx
@@ -16,6 +16,22 @@ export const teaminfo = [
         major: "CS",
         bio: "Hello this a bio for Frank",
         id: 2
+    },
+    {
+        name: "Frank Rodriguez",
+        team: "Dev",
+        class: "2020",
+        major: "CS",
+        bio: "Hello this a bio for Frank",
+        id: 2
+    },
+    {
+        name: "Frank Rodriguez",
+        team: "Dev",
+        class: "2020",
+        major: "CS",
+        bio: "Hello this a bio for Frank",
+        id: 2
     }
 ]
 
