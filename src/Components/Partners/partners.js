d2e29b251d8b63758d389f6261ad986ee25e3be9
Frank <fsr32@cornell.edu>
2018-08-01 19:32:54 -0700

    commented out main

--- src/Components/Partners/partners.js
+++ src/Components/Partners/partners.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import '../../assets/css/main.css';
-import '../../assets/css/noscript.css';
+// import '../../assets/css/main.css';
+// import '../../assets/css/noscript.css';
 
 class Partners extends Component {
     render() {
