e193a5c926011200b12ba8d18c1750df77cdd494
Anirudh Maddula <aam252@cornell.edu>
2018-08-13 01:33:33 -0500

    found a background but its broken styling

--- src/Components/Title/title.jsx
+++ src/Components/Title/title.jsx
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import '../../assets/css/App.css';
 import '../../assets/css/layout.css';
 
+// const background = require('./assets/images/bluesquares.jpg');
 
 function Title(props) {
     return (
         <div class="avenuefont center">
-            <h1>Define your own reality.</h1>
+            <h1 class="erasecss">Define your own reality.</h1>
         </div>
     );
 }
